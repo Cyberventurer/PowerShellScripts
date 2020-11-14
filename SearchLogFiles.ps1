eecd515ef4a022762dbceec6fd1f525bcc52d777
MattLongHappyCoder <longmatthew10@hotmail.com>
2020-11-13 21:44:37 -0500

    Added parameters and help syntax.

--- SearchLogFiles.ps1
+++ SearchLogFiles.ps1
@@ -1,9 +1,23 @@
-$LogPath = Read-Host "Enter the path for the log files: "
+<#
+.SYNOPSIS
+Search log files for a keyword.
+.DESCRIPTION
+Search log files for a keyword and write log entries with the specified keyword to a separate file for analysis.
+.PARAMETER LogPath
+Folder where the log files are stored
+.PARAMETER OutputFile
+The full path and file name of the output file
+.PARAMETER SearchString
+The string to search for
+.EXAMPLE
+C:\Logs C:\Temp\LogEntries.txt "404"
+404 - Not found
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)][string]$LogPath, 
+    [Parameter(Mandatory)][string]$OutputFile, 
+    [Parameter(Mandatory)][string]$SearchString
+)
 
-$OutFile = Read-Host "Enter the full path and file name for the output file: "
-
-$SearchString = Read-Host "Enter the string to search for: "
-
-$Logs = Get-ChildItem -Path $LogPath -Recurse -Include *.log, *.txt
-
-$Logs | Select-String -Pattern $SearchString | Set-Content $OutFile
+Get-ChildItem -Path $LogPath -Recurse -Include *.log, *.txt | Select-String -Pattern $SearchString | Set-Content $OutputFile -Force
